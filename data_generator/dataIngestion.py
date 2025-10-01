6135a456f921787433c057f5d8bfab9353f3c082
Anukul Jena <anukul.jena@gmail.com>
2025-10-01 23:05:58 +0530

    Wrap records in a 'data' field when publishing to Pub/Sub

--- data_generator/dataIngestion.py
+++ data_generator/dataIngestion.py
@@ -28,7 +28,9 @@ def publish_to_pubsub(data, topic_path, data_type_filter=None):
 
         for record in records:
             try:
-                message_bytes = json.dumps(record).encode("utf-8")
+                # Wrap the record in a 'data' field
+                message_payload = {"data": record}
+                message_bytes = json.dumps(message_payload).encode("utf-8")
                 future = publisher.publish(topic_path, message_bytes)
                 future.result(timeout=10)
                 count += 1
