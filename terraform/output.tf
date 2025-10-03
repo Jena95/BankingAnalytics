d9db8316a6a8276b5c2d1c6ab148a9109db715b0
Anukul Jena <anukul.jena@gmail.com>
2025-10-03 22:10:57 +0530

    s

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
 output "pubsub_topic" {
-  value = google_pubsub_topic.demo_topic.name
+  value = google_pubsub_topic.banking_topic.name
 }
 
 output "bigquery_table" {
-  value = "${google_bigquery_dataset.demo_dataset.dataset_id}.${google_bigquery_table.demo_table.table_id}"
+  value = "${google_bigquery_dataset.banking_dataset.dataset_id}.${google_bigquery_table.banking_table.table_id}"
 }
 
 output "subscription_name" {
-  value = google_pubsub_subscription.bigquery_subscription.name
+  value = google_pubsub_subscription.banking_table_subscription.name
 }
