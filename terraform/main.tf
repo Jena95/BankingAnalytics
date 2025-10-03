7cd65dc07a2cdfc84cabf82e29113da34425ff0c
Anukul Jena <anukul.jena@gmail.com>
2025-10-03 12:34:12 +0530

    as

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ resource "google_pubsub_subscription" "banking_subscription" {
   topic = google_pubsub_topic.banking_topic.id
 
   bigquery_config {
-    table            = "projects/${var.project_id}/datasets/banking_raw/tables/raw_banking_data"
+    table            = "projects/brave-reason-421203/datasets/banking_raw/tables/raw_banking_data"
     use_topic_schema = true
     write_metadata   = true
   }
